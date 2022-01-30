d522fdcac9ec99999470585608289c179c7a24fa
AtanasKisyov <atanaskisyov9@gmail.com>
2022-01-30 14:35:09 +0200

    settings and urls update

--- scrap_input/urls.py
+++ scrap_input/urls.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
 from django.urls import path
+from .scrap_app.views import home_page, login_page, input_page, scan_page, overview_page
 
 urlpatterns = [
+    path('', home_page),
     path('admin/', admin.site.urls),
+    path('login/', login_page),
+    path('input/', input_page),
+    path('scan/', scan_page),
+    path('overview/', overview_page),
 ]

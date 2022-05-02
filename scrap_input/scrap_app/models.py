568bbf4f0e1e0d2dbe0ff195dfbbeb191a44a764
AtanasKisyov <atanaskisyov9@gmail.com>
2022-05-02 14:34:21 +0300

    create db models

---
+++ scrap_input/scrap_app/models.py
@@ -0,0 +1,57 @@
+from django.db import models
+
+
+class Production(models.Model):
+
+    PRODUCTION_NAME_MAX_LENGTH = 15
+
+    production_name = models.CharField(
+        max_length=PRODUCTION_NAME_MAX_LENGTH,
+    )
+
+    def __str__(self):
+        return f'{self.production_name}'
+
+
+class Area(models.Model):
+
+    AREA_NAME_MAX_LENGTH = 15
+
+    area_name = models.CharField(
+        max_length=AREA_NAME_MAX_LENGTH,
+    )
+
+    production = models.ForeignKey(
+        Production,
+        on_delete=models.CASCADE,
+    )
+
+    def __str__(self):
+        return f'{self.area_name}'
+
+
+class Material(models.Model):
+
+    MATERIAL_NUMBER_MAX_LENGTH = 30
+    MATERIAL_DESCRIPTION_MAX_LENGTH = 50
+
+    material_number = models.CharField(
+        max_length=MATERIAL_NUMBER_MAX_LENGTH,
+    )
+
+    material_description = models.CharField(
+        max_length=MATERIAL_DESCRIPTION_MAX_LENGTH,
+    )
+
+    material_price = models.FloatField()
+
+    material_weight = models.FloatField()
+
+
+# class ScrapTable(models.Model):
+#
+#     material_number = ''
+#     material_description = ''
+#     scrap_quantity = ''
+#     scrap_price = ''
+#     scrap_weight = ''
--- scrap_input/user/models.py
+++ scrap_input/user/models.py
@@ -1,3 +1,29 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+
+AuthUser = get_user_model()
+
+
+class Profile(models.Model):
+
+    FIRST_NAME_MAX_LENGTH = 15
+    LAST_NAME_MAX_LENGTH = 15
+
+    employee_id = models.IntegerField()
+
+    first_name = models.CharField(
+        max_length=FIRST_NAME_MAX_LENGTH,
+    )
+
+    last_name = models.CharField(
+        max_length=LAST_NAME_MAX_LENGTH,
+    )
+
+    user = models.ForeignKey(
+        AuthUser,
+        on_delete=models.CASCADE,
+    )
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'

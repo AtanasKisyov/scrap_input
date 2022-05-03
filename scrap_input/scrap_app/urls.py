337c009dababb62349b7fe35f338d71b5e9df59e
AtanasKisyov <atanaskisyov9@gmail.com>
2022-05-03 18:33:15 +0300

    update urls

--- scrap_input/scrap_app/urls.py
+++ scrap_input/scrap_app/urls.py
@@ -1,3 +1,8 @@
-urlpatterns = [
+from django.urls import path
 
+from scrap_input.scrap_app.views import OverviewView, InputView
+
+urlpatterns = [
+    path('cockpit/', OverviewView.as_view(), name='cockpit'),
+    path('input/', InputView.as_view(), name='input'),
 ]
--- scrap_input/user/urls.py
+++ scrap_input/user/urls.py
@@ -1,3 +1,7 @@
-urlpatterns = [
+from django.urls import path
 
+from scrap_input.user.views import LoginView, CreateUserView
+
+urlpatterns = [
+    path('create/', CreateUserView.as_view(), name='create_user'),
 ]
